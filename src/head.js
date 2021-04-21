9caf9d8e547770f01e60b79a1d45da77f003196f
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-04-21 16:17:49 +0100

    bundle webpack and index.js file

---
+++ src/head.js
@@ -0,0 +1,5 @@
+let breakfast = 'coffee';
+let lunch = 'fried rice';
+let dinner = 'drink water'
+
+export {breakfast,lunch,dinner}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './css/style.scss'
+import * as meal from './head'
+
+console.log('John what is it?');
+let a = 'Big';
+let b = 'small';
+console.log(a + b);
+
+console.log(meal.lunch);
