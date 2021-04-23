9bcc02115d3ee9eed808540091ad0bca3e8ebe1e
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-04-24 00:47:24 +0100

    Add home.js

--- main.js
+++ main.js
@@ -451,21 +451,40 @@ module.exports = function (list, options) {
 
 /***/ }),
 
-/***/ "./src/head.js":
+/***/ "./src/home.js":
 /*!*********************!*\
-  !*** ./src/head.js ***!
+  !*** ./src/home.js ***!
   \*********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "breakfast": () => (/* binding */ breakfast),
-/* harmony export */   "lunch": () => (/* binding */ lunch),
-/* harmony export */   "dinner": () => (/* binding */ dinner)
+/* harmony export */   "homeFunc": () => (/* binding */ homeFunc)
 /* harmony export */ });
-let breakfast = 'coffee';
-let lunch = 'fried rice';
-let dinner = 'drink water'
+
+const homeFunc = () => {
+  const main = document.createElement('div');
+  main.classList.add('main', 'text-light');
+  const mainDiv = document.createElement('div');
+  mainDiv.classList.add('main-div');
+  const heading = document.createElement('h1');
+  heading.classList.add('home');
+  heading.textContent = 'Kenz Restaurant'
+  const paragraph = document.createElement('p');
+  paragraph.classList.add('home-para');
+  paragraph.textContent = 'Welcome to the home of your favourite delicacies. We serve the best in town. what you eat is what you\'re made of'
+  const btn = document.createElement('button');
+  btn.textContent = 'Menu';
+  btn.classList.add('btn-menu', 'btn', 'btn-danger');
+  
+  mainDiv.appendChild(heading);
+  mainDiv.appendChild(paragraph);
+  mainDiv.appendChild(btn);
+  main.appendChild(mainDiv);
+
+  content.appendChild(main);
+
+}
 
 
 
@@ -597,15 +616,16 @@ var __webpack_exports__ = {};
   \**********************/
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _css_style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./css/style.scss */ "./src/css/style.scss");
-/* harmony import */ var _head__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./head */ "./src/head.js");
-/* harmony import */ var _nav__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./nav */ "./src/nav.js");
+/* harmony import */ var _nav__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./nav */ "./src/nav.js");
+/* harmony import */ var _home__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./home */ "./src/home.js");
 
 
 
 
 const content = document.querySelector('#content');
 
-_nav__WEBPACK_IMPORTED_MODULE_2__.navbar();
+_nav__WEBPACK_IMPORTED_MODULE_1__.navbar();
+_home__WEBPACK_IMPORTED_MODULE_2__.homeFunc();
 })();
 
 /******/ })()
